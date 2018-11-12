c50ba7da4914bcea3a027019f1bd7be245bdb8f8
ChuanShenLive <chuanshen@live.cn>
2018-11-12 21:41:42 +0800

    2018-11-12

--- Chapter_02/stereo-javaconfig/stereo-javaconfig/src/main/java/com/chuanshen/soundsystem/CDPlayerConfig.java
+++ Chapter_02/stereo-javaconfig/stereo-javaconfig/src/main/java/com/chuanshen/soundsystem/CDPlayerConfig.java
@@ -1,8 +1,43 @@
 package com.chuanshen.soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CDPlayerConfig {
 
+	@Bean
+	// @Bean(name="lonelyHeartsClubBand") // 默认同方法名，可以重命名
+	public CompactDisc sgtPeppers() {
+		return new SgtPeppers();
+	}
+	
+	@Bean
+	public CompactDisc randomBeatlesCD() {
+		int choice = (int) Math.floor(Math.random() * 4);
+		if (choice == 0) {
+			return new SgtPeppers();
+		} else if (choice == 1) {
+			return new WhiteAlbum();
+		} else if (choice == 2) {
+			return new HardDaysNight();
+		} else {
+			return new Revolver();
+		}
+	}
+	
+	// @Bean
+	// public CDPlayer cdPlayer() {
+	//  return new CDPlayer(sgtPeppers());
+	// }
+	
+	// @Bean
+	// public CDPlayer cdPlayer(CompactDisc compactDisc) {
+	//	return new CDPlayer(compactDisc);
+	// }
+	
+	@Bean
+	public CDPlayer cdPlayer(CompactDisc sgtPeppers) {
+		return new CDPlayer(sgtPeppers);
+	}
 }
--- Chapter_02/stereo-javaconfig/stereo-javaconfig/src/test/java/com/chuanshen/soundsystem/AppTest.java
+++ Chapter_02/stereo-javaconfig/stereo-javaconfig/src/test/java/com/chuanshen/soundsystem/AppTest.java
@@ -1,4 +1,4 @@
-package com.chuanshen.stereo_javaconfig;
+package com.chuanshen.soundsystem;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
